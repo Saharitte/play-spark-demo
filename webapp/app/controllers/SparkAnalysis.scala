b386c5cdb25da32cec721efa9fe38de5bf817a1c
Richard W. Eggert II <richard.eggert@gmail.com>
2014-09-03 21:05:36 -0400

    implemented SparkAnalysis Controller

---
+++ webapp/app/controllers/SparkAnalysis.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import akka.util.Timeout
+import play.api.libs.concurrent.Akka
+import play.api.libs.json.Json
+import play.api.mvc.{Result, Action, Controller}
+import akka.pattern.ask
+import io.github.reggert.playspark.shared.FileAnalysisMessages._
+import scala.concurrent.duration._
+
+object SparkAnalysis extends Controller {
+
+  implicit val statsWrites = Json.writes[Stats]
+  implicit val timeout = Timeout(5 minutes)
+
+  val broker = Akka.system.actorSelection("akka.tcp://PlayScalaDemo@127.0.0.1:2552/user/PlaySparkBroker")
+
+  def statistics = Action.async {
+    (broker ? RequestStats).mapTo[Stats] map {stats => Ok(Json.toJson(stats))}
+  }
+}
